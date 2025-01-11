c4987e94baa2533a6ac794de100ecd84180ed455
Ching Yi, Chan <chingyichan.tw@gmail.com>
2025-01-11 22:43:19 +0800

    add google-implementation
    Signed-off-by: Ching Yi, Chan <chingyichan.tw@gmail.com>

--- fastapi-google/main.py
+++ fastapi-google/main.py
@@ -1,15 +1,61 @@
 from typing import Union
+from fastapi import FastAPI, Request, Response
+from fastapi.responses import RedirectResponse
+from authlib.integrations.starlette_client import OAuth
+from starlette.config import Config
+from starlette.middleware.sessions import SessionMiddleware
+from starlette.responses import HTMLResponse
 
-from fastapi import FastAPI
-
+config = Config('.env')
 app = FastAPI()
 
+# 添加 session 中間件
+app.add_middleware(SessionMiddleware, secret_key="your-secret-key")
 
-@app.get("/")
-async def read_root():
-    return {"Hello": "World"}
+# 配置 OAuth
+oauth = OAuth(config)
+oauth.register(
+    name='google',
+    server_metadata_url='https://accounts.google.com/.well-known/openid-configuration',
+    client_kwargs={
+        'scope': 'openid email profile'
+    },
+    client_id=config.get('client_id'),
+    client_secret=config.get('client_secret')
+
+)
 
 
-@app.get("/items/{item_id}")
-async def read_item(item_id: int, q: Union[str, None] = None):
-    return {"item_id": item_id, "q": q}
+@app.get("/", response_class=HTMLResponse)
+async def read_root(request: Request):
+    user = request.session.get('user')
+    if user:
+        return f"""
+        <h1>Hello, {user['email']}</h1>
+        <p><a href="/logout">Logout</a></p>
+        {user}
+        """
+    return """
+        <h1>Welcome</h1>
+        <p>Please <a href="/login">login with Google</a></p>
+    """
+
+
+@app.get("/login")
+async def login(request: Request):
+    redirect_uri = request.url_for('auth')  # 這裡設置回調 URL
+    return await oauth.google.authorize_redirect(request, redirect_uri)
+
+
+@app.get("/auth")
+async def auth(request: Request):
+    token = await oauth.google.authorize_access_token(request)
+    userinfo = token['userinfo']
+    request.session['user'] = dict(userinfo)
+    return RedirectResponse(url='/')
+
+
+@app.get("/logout")
+async def logout(request: Request):
+    request.session.pop('user', None)
+    return RedirectResponse(url='/')
